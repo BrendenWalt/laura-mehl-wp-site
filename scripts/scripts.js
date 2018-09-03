92e09984b1ceebc6c6e26ddce0d40068f48b77c1
Brenden W <brendenwalters42@gmail.com>
2018-09-03 17:13:31 -0400

    Scroll to top button functionality implemented

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     console.log ("JS Loaded");
+
+    // Query Selectors
+    var toTop = document.getElementById("toTop");
+    var pageBody = document.getElementsByTagName("body");
+
+    // Click Handlers
+    var onToTopClick = function(event) {
+      event.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }
+
+    // Click Listeners
+    toTop.addEventListener("click", onToTopClick);
+
 });
 
 
