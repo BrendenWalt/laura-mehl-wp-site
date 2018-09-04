6df70b81d9cdcc3aec2ae272b7ce4828c71e5904
Brenden <brenden.walters@wpromote.com>
2018-09-04 15:01:40 -0400

    Added functionality to the hamburger menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,10 +2,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log ("JS Loaded");
 
     // Query Selectors
+    var hamburger = document.getElementById("mobile-nav");
+    var links = document.getElementById("nav-links");
     var toTop = document.getElementById("toTop");
     var pageBody = document.getElementsByTagName("body");
 
     // Click Handlers
+    var onHamburgerClick = function(event) {
+      event.preventDefault();
+      console.log("Hamburger clicked");
+      console.log(links);
+      links.classList.add("mobile-menu");
+    }
+
     var onToTopClick = function(event) {
       event.preventDefault();
       window.scrollTo({
@@ -15,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
     // Click Listeners
+    hamburger.addEventListener("click", onHamburgerClick);
     toTop.addEventListener("click", onToTopClick);
 
 });
